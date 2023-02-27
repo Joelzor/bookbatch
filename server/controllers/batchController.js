42c200064b54e7fa808fbf80495fdd2505adadfb
Joel Watkins <eck087@yahoo.co.uk>
2023-02-27 11:38:36 +0000

    get all batches endpoint added

--- server/controllers/batchController.js
+++ server/controllers/batchController.js
@@ -1,2 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+
+const getAllBatches = async (req, res) => {
+  const batches = await prisma.batch.findMany({
+    include: {
+      books: true,
+      tags: true,
+      posts: true,
+      User: true,
+    },
+  });
+
+  res.status(200).json(batches);
+};
+
+module.exports = { getAllBatches };
--- server/routes/batches.js
+++ server/routes/batches.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const { getAllBatches } = require("../controllers/batchController");
+
+router.route("/").get(getAllBatches);
 
 module.exports = router;
