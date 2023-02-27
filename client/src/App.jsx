e7b78e3bf7fbbaddfef8eaa0e0f8b6767b29d6cf
Joel Watkins <eck087@yahoo.co.uk>
2023-02-27 12:17:32 +0000

    added bootstrap file and set up basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>Header</div>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/login" element={<h1>Log in</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/newbatch" element={<h1>Create new batch</h1>} />
+        <Route path="/batches" element={<h1>Browse batches</h1>} />
+        <Route path="/batches/:id" element={<h1>View batch</h1>} />
+        <Route path="/profile" element={<h1>My profile</h1>} />
+        <Route path="*" element={<h1>Error!</h1>} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
