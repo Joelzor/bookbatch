2e4a41cee2a2031c297a6c90dd5a64a1d3c1a266
Joel Watkins <eck087@yahoo.co.uk>
2023-03-01 17:12:28 +0000

    added search book form to modal

--- client/src/components/CreateBatch.jsx
+++ client/src/components/CreateBatch.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { useGlobalContext } from "../context/auth";
 import "../styles/batch.css";
-import { Container, Modal, Button } from "react-bootstrap";
+import { Container, Modal, Button, Form } from "react-bootstrap";
 import Batch from "./Batch";
 
 const CreateBatch = () => {
   const { loggedInUser } = useGlobalContext();
   const [showBookModal, setShowBookModal] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleClose = () => setShowBookModal(false);
   const handleShow = () => setShowBookModal(true);
@@ -21,17 +22,27 @@ const CreateBatch = () => {
           <Batch handleShow={handleShow} />
           <Modal show={showBookModal} onHide={handleClose}>
             <Modal.Header closeButton>
-              <Modal.Title>Add book</Modal.Title>
+              <Modal.Title>Find book</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              Woohoo, you're reading this text in a modal!
+              <Form className="search-form-modal">
+                <Form.Group controlId="query">
+                  <Form.Control
+                    required
+                    type="search"
+                    name="query"
+                    value={searchQuery}
+                    placeholder="Search for a book..."
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className="searchbar"
+                  />
+                </Form.Group>
+                <Button variant="primary">Search</Button>
+              </Form>
             </Modal.Body>
             <Modal.Footer>
               <Button variant="secondary" onClick={handleClose}>
                 Close
-              </Button>
-              <Button variant="primary" onClick={handleClose}>
-                Save Changes
               </Button>
             </Modal.Footer>
           </Modal>
