639357749f277bec6e6f1eeac1980a97be1e1126
Joel Watkins <eck087@yahoo.co.uk>
2023-03-08 11:02:49 +0000

    Browse page form setup

--- client/src/pages/Browse.jsx
+++ client/src/pages/Browse.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Container, Form, Row, Col, Button, Stack } from "react-bootstrap";
+import PublishedBatch from "../components/PublishedBatch";
+import "../styles/browse.css";
+
 const Browse = () => {
-  return <div>Browse</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col xs={9}>
+          <Form className="mt-4 search-form" onSubmit={handleSubmit}>
+            <Form.Control
+              type="search"
+              value={searchQuery}
+              placeholder="Search batch title or tags..."
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <Button type="submit" variant="info">
+              Submit
+            </Button>
+          </Form>
+          <h4 className="search-heading">Search results</h4>
+          <Stack gap={4}></Stack>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Browse;
