9d7ad9aaaa94e63698f43059f9937b860e3d0786
Joel Watkins <eck087@yahoo.co.uk>
2023-03-23 17:27:59 +0000

    added some quick media queries for HomeIntro

--- client/src/pages/HomeIntro.jsx
+++ client/src/pages/HomeIntro.jsx
@@ -1,17 +1,17 @@
-import { Stack, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const HomeIntro = () => {
   return (
     <section className="background">
-      <Stack className="welcome-container">
+      <div className="welcome-container">
         <h1 className="hero-title">WELCOME TO BOOKBATCH</h1>
         <Link to="/register">
           <Button variant="primary" className="sign-up-btn">
             Sign up now
           </Button>
         </Link>
-      </Stack>
+      </div>
     </section>
   );
 };
